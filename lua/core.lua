99e4e5fc2db5707911fc1bbcf0e96d874920403b
max96at <max96at@gmail.com>
2013-04-15 19:11:44 +0300

    Tried to fix a bug in require.

--- lua/core.lua
+++ lua/core.lua
@@ -1,54 +1,56 @@
 do 
-    local libpath = class.io.File.combine(paths.LUADIR, "lib");
+    local libpath = class.io.File.combine(paths.LUADIR, "lib")
     local function require_impl(lib)
-        local file = class.io.File(libpath, lib..".lua");
-        --local r = class.io.InputStream(file);
-        --local f, m = load(r:readAll(), "lib: "..lib, "t" --[[no bytecode]], _ENV);
+        local file = class.io.File(libpath, lib..".lua")
+        --local r = class.io.InputStream(file)
+        --local f, m = load(r:readAll(), "lib: "..lib, "t" --[[no bytecode]], _ENV)
         --if not f then error(m); end
-        --f();
-        dofile(file:getAbsName());
+        --f()
+        libf = assert(loadfile(file:getAbsName()))
+        setfenv(libf, _G)
+        return libf()
     end
     function require(...)
-        local ret = {};
+        local ret = {}
         for k, v in ipairs({...}) do
-            ret[k] = require_impl(v);
+            ret[k] = require_impl(v)
         end
-        return unpack(ret);
+        return unpack(ret)
     end
     table.indexof = function (t, v)
-        local ret = {};
+        local ret = {}
         for k, v_ in pairs(t) do
             if v == v_ then
-                table.insert(ret, k);
+                table.insert(ret, k)
             end
         end
-        return unpack(ret);
+        return unpack(ret)
     end
 
     function _() end
 end
 
 -------------------------------------------------
-print("Core: ", _ENV);
-require ("LCS", "EventBus", "IrcClient", "IrcServer");
-local eventBus = EventBus();
+print("Core: ", _ENV)
+require ("LCS", "EventBus", "IrcClient", "IrcServer")
+local eventBus = EventBus()
 Core = {
     EVENT_BUS = eventBus,
     VERSION = "0.1"
-};
+}
 
 
 function rehash()
 
     -- todo load config
 
-    local port = 1234;
+    local port = 1234
 
-    eventBus:post("rehash");
+    eventBus:post("rehash")
     if mainserver then eventBus:post("preServerClose"); mainserver:close(); end
-    mainserver = class.net.ServerSocket(port);
-    mainserver:on("accept", newClient);
-    mainserver:setTextMode(true);
+    mainserver = class.net.ServerSocket(port)
+    mainserver:on("accept", newClient)
+    mainserver:setTextMode(true)
 
 end
 
